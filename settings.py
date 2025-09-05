efdf159773030ba201d72626ca6a3c8e6c690168
Nikita Davydov <davydovnikitav@gmail.com>
2025-09-05 13:51:17 +0300

    first commit

---
+++ settings.py
@@ -0,0 +1,11 @@
+class Settings:
+    """Класс для хранения всех настроек игры Alien Invasion."""
+    def __init__(self):
+        """Инициирует настройки игры."""
+        # Параметры экрана
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (0, 0, 0)
+
+        # Настройки корабля
+        self.ship_speed_factor = 1.5
